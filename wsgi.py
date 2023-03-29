9500a21243c34ffb524c0e54f4da58d1b596f9cb
Fabricio <fabricio.bizotto@gmail.com>
2023-03-29 17:35:51 -0300

    asd

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,10 @@
 from app import create_app
 
 app = create_app()
+
+if __name__ == '__main__':
+  # from gevent.pywsgi import WSGIServer
+  # http_server = WSGIServer(('0.0.0.0', 8080), app)
+  # http_server.serve_forever()
+#   app.run(host='0.0.0.0', port=8181, debug=True)
+  app.run()
