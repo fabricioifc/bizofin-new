3cf60a27eafc28f3c6f2f0eb3bc8d2f41e205b87
Fabricio <fabricio.bizotto@gmail.com>
2023-03-29 17:18:58 -0300

    asd

--- config.py
+++ config.py
@@ -37,6 +37,7 @@ class TestingConfig(Config):
 
 class ProductionConfig(Config):
   FLASK_ENV = 'production'
-  SQLALCHEMY_DATABASE_URI = os.getenv('PROD_DATABASE_URI',
-                                      default="sqlite:///" +
-                                      os.path.join(BASE_DIR, 'prod.db'))
+  SQLALCHEMY_DATABASE_URI = os.getenv('JAWSDB_MARIA_URL')
+  # SQLALCHEMY_DATABASE_URI = os.getenv('JAWSDB_MARIA_URL',
+  #                                     default="sqlite:///" +
+  #                                     os.path.join(BASE_DIR, 'prod.db'))
