f645366ef832b780a8707f085776c3ecb7089cb4
Fabricio <fabricio.bizotto@gmail.com>
2023-03-30 10:16:20 -0300

    production model string length

--- app/models/user.py
+++ app/models/user.py
@@ -8,8 +8,8 @@ class User(UserMixin, db.Model):
   __tablename__ = "users"
 
   id = db.Column(db.Integer, primary_key=True)
-  email = db.Column(db.String, unique=True, nullable=False)
-  password = db.Column(db.String, nullable=False)
+  email = db.Column(db.String(255), unique=True, nullable=False)
+  password = db.Column(db.String(255), nullable=False)
   created_on = db.Column(db.DateTime, nullable=False)
   is_admin = db.Column(db.Boolean, nullable=False, default=False)
 
