e15d45be49abe2f7ed7cdd92223c7f4cf15d2915
Fabricio <fabricio.bizotto@gmail.com>
2023-03-29 21:13:21 -0300

    asd

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,9 @@
 from app import create_app
+from app.extensions import db
 
 app = create_app()
+
+db.create_all()
 # if __name__ == '__main__':
 #   # from gevent.pywsgi import WSGIServer
 #   # http_server = WSGIServer(('0.0.0.0', 8080), app)
