d4f09ec681f495f369ee5c56a4181a75d4425c8f
Fabricio <fabricio.bizotto@gmail.com>
2023-03-30 10:40:07 -0300

    prod

--- wsgi.py
+++ wsgi.py
@@ -4,8 +4,8 @@ app = create_app()
 
 with app.app_context():
     from app.extensions import db
-    
-    print('Excluindo tabelas')
-    db.drop_all()
+
+    # print('Excluindo tabelas')
+    # db.drop_all()
     print('Criando tabelas')
     db.create_all()
